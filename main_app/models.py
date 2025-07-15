8ae11b7d0a99c26e977669263bec188518500573
Mikebrown0409 <mikebrown0409@gmail.com>
2025-07-15 13:18:05 -0700

    Fix delete functionality and journal text.

--- main_app/models.py
+++ main_app/models.py
@@ -4,22 +4,24 @@ from django.urls import reverse
 import datetime
 
 INTENSITY_LEVEL = (
-  ('1', 'Subtle'),
-  ('2', 'Uncomfortable'),
-  ('3', 'Moderate'),
-  ('4', 'Strong'),
-  ('5', 'Intense'),
+    ("1", "Subtle"),
+    ("2", "Uncomfortable"),
+    ("3", "Moderate"),
+    ("4", "Strong"),
+    ("5", "Intense"),
 )
+
+
 # Create your models here.
 class MoodEntry(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  mood = models.CharField(max_length=100)
-  intensity = models.CharField(max_length=100, choices=INTENSITY_LEVEL)
-  journal_text = models.TextField(max_length=250)
-  created_at = models.DateField('Journal Date', default=datetime.date.today)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mood = models.CharField(max_length=100)
+    intensity = models.CharField(max_length=100, choices=INTENSITY_LEVEL)
+    journal_text = models.TextField(max_length=250)
+    created_at = models.DateField("Journal Date", default=datetime.date.today)
 
-  def __str__(self):
-    return f"{self.name} ({self.id})"
+    def __str__(self):
+        return f"{self.mood} ({self.id})"
 
-  def get_absolute_url(self):
-    return reverse('mood-detail', kwargs={'pk': self.pk})
+    def get_absolute_url(self):
+        return reverse("mood-detail", kwargs={"pk": self.pk})
